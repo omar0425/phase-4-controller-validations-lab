7625c550c3d970af1446392303b6349c7c28bf9e
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-11-24 21:08:40 -0500

    commit tree 1ece2caf74c189f5e2b9de8431da998213269a27

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,15 +2,19 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
     author = Author.create(author_params)
-
+  if author.valid?
     render json: author, status: :created
+  else
+    render json: { errors: author.errors }, status: :unprocessable_entity
   end
+end
+
+
 
   private
   
